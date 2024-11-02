d5608cf69b88454c57a9dea86f4783f71820b6e6
= <alexandre.roussel.48@gmail.com>
2024-11-02 16:27:37 +0100

    chore: auto generated files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+//import { UsersListComponent } from './components/user/users-list/users-list.component';
+//import { UsersDetailsComponent } from './components/user/users-details/users-details.component';
+//import { UsersFormComponent } from './components/user/users-form/users-form.component';
+//import { PageNotFoundComponent } from './components/shared/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  //	{path: 'users', component: UsersListComponent},
+  //	{path: 'user/:userId', component: UsersDetailsComponent},
+  //	{path: 'user/edit/:userId', component: UsersFormComponent},
+  { path: '', component: AppComponent },
+  //	{path: '**', component: PageNotFoundComponent}
+];
