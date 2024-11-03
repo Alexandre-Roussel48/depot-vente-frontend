ec0fa1902b5b103c5fffd33503a03a2224599143
= <alexandre.roussel.48@gmail.com>
2024-11-03 20:34:30 +0100

    feat: gestion login component

--- src/app/components/gestion/gestion.component.ts
+++ src/app/components/gestion/gestion.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-gestion',
   standalone: true,
-  imports: [],
+  imports: [LoginComponent],
   templateUrl: './gestion.component.html',
   styleUrl: './gestion.component.scss',
 })
-export class GestionComponent {}
+export class GestionComponent {
+  isAuthenticated: boolean = false;
+
+  constructor(public as: AuthService) {}
+
+  ngOnInit() {}
+
+  updateAuth(value: boolean) {
+    this.isAuthenticated = value;
+  }
+}
---
+++ src/app/components/gestion/login/login.component.ts
@@ -0,0 +1,57 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+import { Observable } from 'rxjs';
+import { AuthService } from '../../../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  login$!: Observable<{ isAuthenticated: boolean; message: string }>;
+  message!: string;
+  @Output() authEmitter = new EventEmitter<boolean>();
+
+  constructor(
+    private fb: FormBuilder,
+    public as: AuthService
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      this.login$ = this.as.login(email, password);
+      this.login$.subscribe((login) => {
+        if (login) {
+          this.message = login.message;
+          this.authEmitter.emit(login.isAuthenticated);
+        }
+      });
+    }
+  }
+}
