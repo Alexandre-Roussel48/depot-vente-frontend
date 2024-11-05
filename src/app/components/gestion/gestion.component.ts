7e116958eb7926a95332e227c2c9d60eca34dc6d
= <alexandre.roussel.48@gmail.com>
2024-11-05 21:28:42 +0100

    feat: refactored gestion dashboard on one component

--- src/app/components/gestion/gestion.component.ts
+++ src/app/components/gestion/gestion.component.ts
@@ -1,22 +1,55 @@
 import { Component } from '@angular/core';
-import { LoginComponent } from './login/login.component';
-import { AuthService } from '../../services/auth.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-gestion',
   standalone: true,
-  imports: [LoginComponent],
+  imports: [MatCardModule, MatIconModule, RouterLink],
   templateUrl: './gestion.component.html',
   styleUrl: './gestion.component.scss',
 })
 export class GestionComponent {
-  isAuthenticated: boolean = false;
+  constructor() {}
 
-  constructor(public as: AuthService) {}
-
-  ngOnInit() {}
-
-  updateAuth(value: boolean) {
-    this.isAuthenticated = value;
-  }
+  items = [
+    {
+      icon: 'shopping_cart',
+      title: 'Vendre',
+      description: 'Enregistre une vente dans le système.',
+      route: '/gestion/sell',
+    },
+    {
+      icon: 'inventory_2',
+      title: 'Déposer',
+      description: 'Enregistrer un dépôt dans le système.',
+      route: '/gestion/deposit',
+    },
+    {
+      icon: 'attach_money',
+      title: 'Bilans',
+      description: 'Outil de consultation des bilans généraux.',
+      route: '/gestion/balance',
+    },
+    {
+      icon: 'info',
+      title: 'Infos vendeurs',
+      description:
+        'Outil de consultation des informations individuelles des vendeurs.',
+      route: '/gestion/infos',
+    },
+    {
+      icon: 'layers',
+      title: 'Stocks',
+      description: 'Outil de consultation des stocks.',
+      route: '/gestion/stocks',
+    },
+    {
+      icon: 'list',
+      title: 'Transactions',
+      description: 'Outil de consultation des transactions.',
+      route: '/gestion/transactions',
+    },
+  ];
 }
